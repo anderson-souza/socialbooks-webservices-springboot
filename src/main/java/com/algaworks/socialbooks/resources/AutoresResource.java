970e46db187cd7abaa650c4511d4f0a2a5ddcb1b
Anderson <andersonpds14@gmail.com>
2019-01-25 22:44:15 -0200

    Adicionado a possibilidade de retorno XML na consulta de Autores

--- src/main/java/com/algaworks/socialbooks/resources/AutoresResource.java
+++ src/main/java/com/algaworks/socialbooks/resources/AutoresResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +26,8 @@ public class AutoresResource {
 	@Autowired
 	AutoresService autoresService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<List<Autor>> listar() {
 		return ResponseEntity.status(HttpStatus.OK).body(autoresService.listar());
 	}
